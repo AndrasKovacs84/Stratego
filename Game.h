65b118413f8c145b49a117b198d1eec7d73b5c22
Unknown <ka841001@gmail.com>
2018-01-15 06:15:35 +0100

    Removed code that was copied to new class

--- Game.h
+++ Game.h
@@ -22,19 +22,11 @@ private:
 
     //Game init
     void initButtons(); //TODO some UI init with menu
-    //void initGameArea(); //TODO UI init class
-    //void initDiscardPile(); //TODO UI init class
-    //void initCardArea();    //TODO UI init class
 
     //Main Game Loop
     void delegateAccordingToGameState();    //TODO will need one of these per game mode
     void populateCardArea();    //TODO GAME area class
     void spawnNrOfTypesOfCards(CardType typeToSpawn, int amountToSpawn, Color color);
-    //void placeToNextEmptyFieldInSideArea(std::unique_ptr<Card> cardToPlace);
-    //void initCardPositions();
-
-    //void moveCard();
-    //bool isCardAreaEmpty();
     void checkIfTied(); //Functions used in it are moved to the GameArea object.
 
     //Game state progressing functions, they also contain some other actions as well...
@@ -51,9 +43,6 @@ private:
     void renderCardArea();
     void renderDiscardPile();
     void renderAvailableMoves();
-    //void clearHighlights();
-    //void changeFacingOfCards(Color color, bool faceDown);
-    //void revealCombatants();
 
     //Input
     void handlePlayerClicks(); // get player click from display, delegate to input processing function based on game state, progress game state
@@ -62,12 +51,7 @@ private:
 
 
     //Battle phase input
-    //bool playerHasValidMoves(Color color);
-    //std::vector<int> gatherNearbyValidFieldIndeces(unsigned char moveDist, int index, Color color);
-    //void gatherViableMovesInDir(int index, unsigned char moveDist, Color color, std::vector<int> &result, Direction dir);
-    //void resolveBattle();
     void triggerVictory(Color color); //Does nothing but sets game state to victory for color
-    //int getNextEmptyDiscardPileIndex();
 
 
     std::vector<int> possibleMoves;
